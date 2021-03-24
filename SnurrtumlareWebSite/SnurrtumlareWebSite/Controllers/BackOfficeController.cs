2eb3e844c23e9d82aac19de012c6b0ce35519098
michael sava <savamichael@hotmail.com>
2021-03-24 17:05:20 +0100

    Added BackOfficeController

---
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/BackOfficeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnurrtumlareWebSite.Controllers
+{
+    public class BackOfficeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Customers()
+        {
+            return View();
+        }
+
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+    }
+}
