a23c45c0ee3f975f7518b0fef0a971c07598f4b6
michael sava <savamichael@hotmail.com>
2021-03-30 10:26:39 +0200

    Added models

---
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Models/AdminUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnurrtumlareWebSite.Models
+{
+    public class AdminUser
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnurrtumlareWebSite.Models
+{
+    public class Product
+    {
+        public int PruductId { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public string ProductDescription { get; set; }
+        public int AmountInStock { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
