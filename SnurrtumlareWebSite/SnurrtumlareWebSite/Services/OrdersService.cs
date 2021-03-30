9ce40211081cb6923c2ddc169111f3630d01cace
Fredrik Tvingstedt <mrt86@live.se>
2021-03-30 15:16:09 +0200

    Order-Service
    Not finish

---
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Services/OrdersService.cs
@@ -0,0 +1,34 @@
+﻿using SnurrtumlareWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnurrtumlareWebSite.Services
+{
+    public class OrdersService
+    {
+        DummyData dummyData = new DummyData();
+
+        public List<OrderModel> Get()
+
+        { 
+            return dummyData.TempOrders; 
+        }
+       
+      
+public class DummyData
+        {
+            public DummyData()
+            {
+            }
+
+            public List<OrderModel> TempOrders { get; internal set; }
+        }
+    }
+
+   
+}
+      
+
+
