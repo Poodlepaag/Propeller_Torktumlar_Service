60b3ec47a79a027c914e1071b802ae1ae27e3855
giddster <73110741+giddster@users.noreply.github.com>
2021-04-01 11:43:49 +0200

    Update User.cs
    changed propname from CustomerId to UserID

--- SnurrtumlareWebSite/SnurrtumlareWebSite/Models/User.cs
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Models/User.cs
@@ -7,7 +7,7 @@ namespace SnurrtumlareWebSite.Models
 {
     public class User
     {
-        public int CustomerId { get; set; }
+        public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
