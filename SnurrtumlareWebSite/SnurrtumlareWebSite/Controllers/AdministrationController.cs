56472f1731f7669ed2ffac92b07ffb97c77c34d8
michael sava <savamichael@hotmail.com>
2021-04-05 15:42:35 +0200

    Added functionality to Identity Administration
    Added functionality to bound a Role to a specific User

--- SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/AdministrationController.cs
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/AdministrationController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SnurrtumlareWebSite.ViewModels;
+using SnurrtumlareWebSite.Views.Administration;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SnurrtumlareWebSite.Controllers
@@ -69,7 +71,7 @@ namespace SnurrtumlareWebSite.Controllers
 
             if (role == null)
             {
-                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                ViewBag.ErrorMessage = $"Role with Id: {id} cannot be found";
 
                 return View("NotFoundAdministration");
             }
@@ -98,7 +100,7 @@ namespace SnurrtumlareWebSite.Controllers
 
             if (role == null)
             {
-                ViewBag.ErrorMessage = $"Role with Id = {model.Id} cannot be found";
+                ViewBag.ErrorMessage = $"Role with Id: {model.Id} cannot be found";
                 return View("NotFound");
             }
             else
@@ -121,6 +123,87 @@ namespace SnurrtumlareWebSite.Controllers
                 return View(model);
             }
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with id: {roleId} cannot be found";
+
+                return View("NotFoundAdministration");
+            }
+
+            var model = new List<UserRoleViewModel>();
+
+            foreach (var user in userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel { UserId = user.Id, UserName = user.UserName };
+
+                //if (await userManager.IsInRoleAsync(user, role.Name))
+                //    userRoleViewModel.IsSelected = true;
+                //else
+                //    userRoleViewModel.IsSelected = false;
+
+                userRoleViewModel.IsSelected = (await userManager.IsInRoleAsync(user, role.Name)) ? true : false;
+
+
+                model.Add(userRoleViewModel);
+            }
+
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            for (int i = 0; i < model.Count; i++)
+            {
+                var user = await userManager.FindByIdAsync(model[i].UserId);
+
+                IdentityResult result = null;
+
+                if (model[i].IsSelected && !(await userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!model[i].IsSelected && await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (result.Succeeded)
+                {
+                    if (i < (model.Count - 1))
+                        continue;
+                    else
+                        return RedirectToAction("EditRole", new { Id = roleId });
+                }
+            }
+
+            return RedirectToAction("EditRole", new { Id = roleId });
+        }
+
+
 
     }
 }
