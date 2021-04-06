84d157c8bf8ccc11410b307c2036f97d5a9c9096
Daniel <danielriddersporre@gmail.com>
2021-04-06 18:37:46 +0200

    Revert "Update Program.cs"
    This reverts commit a9646ccd5dd62199324b716e3f0c5a93856183f7.

--- SnurrtumlareWebSite/SnurrtumlareWebSite/Program.cs
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Program.cs
@@ -15,11 +15,7 @@ namespace SnurrtumlareWebSite
         public static void Main(string[] args)
         {
             //CreateHostBuilder(args).Build().Run();
-<<<<<<< HEAD
             CreateHostBuilder(args).Build().MigrateDb().Run();
-=======
-            CreateHostBuilder(args).Build().MigrateDb().MigrateSnurrtumlareDb().Run();
->>>>>>> parent of cdc0dd9 (Minor changes)
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
