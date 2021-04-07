6fba95c77ed40c386ee480306aadd0e2993053ed
michael sava <savamichael@hotmail.com>
2021-04-07 13:04:15 +0200

    Added profileView in BackOffice
    Added ProfileView
    Deleted Old Files

--- SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/BackOfficeController.cs
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/BackOfficeController.cs
@@ -20,25 +20,5 @@ namespace SnurrtumlareWebSite.Controllers
         {
             return View();
         }
-
-        public IActionResult Products(string searchString)
-        {
-            return View(_productsService.Get(searchString));
-        }
-
-        public IActionResult Orders()
-        {
-            return View(_ordersService.Get());
-        }
-
-        public IActionResult Profile(User user)
-        {
-            return View(user);
-        }
-
-        public IActionResult Users()
-        {
-            return View(_usersService.GetUsers());
-        }
     }
 }
---
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/BackOfficeProfileController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SnurrtumlareWebSite.Data;
+using SnurrtumlareWebSite.Models;
+
+namespace SnurrtumlareWebSite.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class BackOfficeProfileController : Controller
+    {
+        private readonly SnurrtumlareDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public BackOfficeProfileController(SnurrtumlareDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: BackOfficeProfile
+        public async Task<IActionResult> Index()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            return View(await _context.Users.Where(u => u.Email == userEmail).ToListAsync());
+        }
+
+        // GET: BackOfficeProfile/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        //// GET: BackOfficeProfile/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: BackOfficeProfile/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("UserId,FirstName,LastName,Address,ZipCode,City,Email,Phone")] User user)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(user);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(user);
+        //}
+
+        // GET: BackOfficeProfile/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: BackOfficeProfile/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,FirstName,LastName,Address,ZipCode,City,Email,Phone")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        //// GET: BackOfficeProfile/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var user = await _context.Users
+        //        .FirstOrDefaultAsync(m => m.UserId == id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(user);
+        //}
+
+        //// POST: BackOfficeProfile/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var user = await _context.Users.FindAsync(id);
+        //    _context.Users.Remove(user);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+    }
+}
