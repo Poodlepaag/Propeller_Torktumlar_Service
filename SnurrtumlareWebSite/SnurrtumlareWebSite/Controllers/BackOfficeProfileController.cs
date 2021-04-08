f34b3c874b98cbf2024a85d6ef4fa2287ab85322
michael sava <savamichael@hotmail.com>
2021-04-08 18:56:23 +0200

    Update BackOfficeProfileController.cs
    Code CleanUp

--- SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/BackOfficeProfileController.cs
+++ SnurrtumlareWebSite/SnurrtumlareWebSite/Controllers/BackOfficeProfileController.cs
@@ -56,29 +56,6 @@ namespace SnurrtumlareWebSite.Controllers
             return View(user);
         }
 
-        #region OLD
-        //// GET: BackOfficeProfile/Create
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: BackOfficeProfile/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("UserId,FirstName,LastName,Address,ZipCode,City,Email,Phone")] User user)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(user);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(user);
-        //}
-        #endregion
 
         // GET: BackOfficeProfile/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -133,44 +110,6 @@ namespace SnurrtumlareWebSite.Controllers
 
             return View(user);
         }
-
-
-        #region OLD
-        //// GET: BackOfficeProfile/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var user = await _context.Users
-        //        .FirstOrDefaultAsync(m => m.UserId == id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(user);
-        //}
-
-        //// POST: BackOfficeProfile/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var user = await _context.Users.FindAsync(id);
-        //    _context.Users.Remove(user);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-
-        //public bool UserExists(int id)
-        //{
-        //    return _context.Users.Any(e => e.UserId == id);
-        //}
-        #endregion
 
     }
 }
